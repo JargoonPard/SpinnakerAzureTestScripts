c473ef5bea56247fb68bc5215dbf042e185bb062
JargoonPard <larry.guger@hotmail.com>
2016-01-22 13:30:39 -0800

    Added sleep

--- CloudDriverLoadBalancerE2E.py
+++ CloudDriverLoadBalancerE2E.py
@@ -3,7 +3,8 @@ import adal
 import requests
 import os
 import json
-import time
+from time import sleep, ctime
+import sys
 
 authority = os.environ['AZURE_E2E_AUTHORITY']
 client_id = os.environ['AZURE_E2E_CLIENT_ID']
@@ -20,8 +21,9 @@ access_token = token_response.get('accessToken')
 headers = {"Authorization": 'Bearer ' + access_token}
 
 load_balancer_endpoint = 'https://management.azure.com/subscriptions/0b44b9a8-ee7f-4020-a239-8b498e6fd0a2/resourceGroups/azure1_westus/providers/Microsoft.Network/loadBalancers/azureMASM-st1-d11?api-version=2015-05-01-preview'
+deployment_endpoint = 'https://management.azure.com/subscriptions/0b44b9a8-ee7f-4020-a239-8b498e6fd0a2/resourceGroups/azure1_westus/providers/microsoft.resources/deployments/azureMASM-st1-d11_deployment?api-version=2015-05-01-preview'
 
-print('Check for existing load balancer')
+print 'Check for existing load balancer at: ', ctime()
 r = requests.get(load_balancer_endpoint, headers=headers)
 
 json_output = r.json()
@@ -31,22 +33,22 @@ json_output = r.json()
 json_output['error']
 
 #create a new loadbalancer through clouddriver
-#curl -X POST -H "Content-Type: application/json" -d '[ { "upsertLoadBalancer": { "cloudProvider" : "azure", "appName" : "azure1", "loadBalancerName" : "azure1-st1-d1", "stack" : "st1", "detail" : "d1", "credentials" : "azure-cred1", "region" : "West US", "vnet" : null, "probes" : [ { "probeName" : "healthcheck1", "probeProtocol" : "HTTP", "probePort" : 7001, "probePath" : "/healthcheck", "probeInterval" : 10, "unhealthyThreshold" : 2 } ], "securityGroups" : null, "loadBalancingRules" : [ { "ruleName" : "lbRule1", "protocol" : "TCP", "externalPort" : "80", "backendPort" : "80", "probeName" : "healthcheck1", "persistence" : "None", "idleTimeout" : "4" } ], "inboundNATRules" : [ { "ruleName" : "inboundRule1", "serviceType" : "SSH", "protocol" : "TCP", "port" : "80" } ], "name" : "azure1-st1-d1", "user" : "[anonymous]" }} ]' localhost:7002/ops
-
 url = clouddriver_host + '/ops'
 lb_data = '[ { "upsertLoadBalancer": { "cloudProvider" : "azure", "appName" : "azure1", "loadBalancerName" : "azure1-st1-d1", "stack" : "st1", "detail" : "d1", "credentials" : "azure-cred1", "region" : "West US", "vnet" : null, "probes" : [ { "probeName" : "healthcheck1", "probeProtocol" : "HTTP", "probePort" : 7001, "probePath" : "/healthcheck", "probeInterval" : 10, "unhealthyThreshold" : 2 } ], "securityGroups" : null, "loadBalancingRules" : [ { "ruleName" : "lbRule1", "protocol" : "TCP", "externalPort" : "80", "backendPort" : "80", "probeName" : "healthcheck1", "persistence" : "None", "idleTimeout" : "4" } ], "inboundNATRules" : [ { "ruleName" : "inboundRule1", "serviceType" : "SSH", "protocol" : "TCP", "port" : "80" } ], "name" : "azure1-st1-d1", "user" : "[anonymous]" }} ]'
 
-print('Post new load balancer')
+print ctime(), ' - Post new load balancer'
 r = requests.post(url, data = lb_data, headers={'Content-Type': 'application/json'})
 print(r.text)
 
 #Hopefully 2 minutes is enough time for Azure to provision the loadbalancer
 #Could replace this with a test of the deployment status to make it more robust.
-print('sleeping for 2 minutes...')
-time.sleep(120)
-
-print('Check for new load balancer')
+print('sleeping for 120 seconds then check for new load balancer')
+print 'start sleep at: ', ctime()
+sleep(120)
+print 'end sleep at: ', ctime()
 r = requests.get(load_balancer_endpoint, headers=headers)
+print ctime(), ' - finished checking deployment'
+
 print(r.text)
 
 print('SUCCESS!!')
