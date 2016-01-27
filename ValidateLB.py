9fb82607df880cdc17dabd1c8bd5b6abdcca1e43
JargoonPard <larry.guger@hotmail.com>
2016-01-27 07:51:08 -0800

    Removed subscription specific details

--- ValidateLB.py
+++ ValidateLB.py
@@ -9,18 +9,19 @@ import sys
 authority = os.environ['AZURE_E2E_AUTHORITY']
 client_id = os.environ['AZURE_E2E_CLIENT_ID']
 client_secret = os.environ['AZURE_E2E_CLIENT_SECRET']
+subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
 clouddriver_host = 'http://localhost:7002'
 
 token_response = adal.acquire_token_with_client_credentials(
-	'https://login.microsoftonline.com/b06e36c3-7474-4870-96c2-32e263b6344c',
-	'3895ac6f-b20c-4055-bff8-71abdfd18bfd',
-	'pYhDKB57D+boBm9kB3Up8L5WOaWfkdqQH7I0wm5mLCs='
+	authority,
+	client_id,
+	client_secret
 )
 
 access_token = token_response.get('accessToken')
 headers = {"Authorization": 'Bearer ' + access_token}
 
-load_balancer_endpoint = 'https://management.azure.com/subscriptions/0b44b9a8-ee7f-4020-a239-8b498e6fd0a2/resourceGroups/azure1-westus/providers/Microsoft.Network/loadBalancers/azure1-st1-d1?api-version=2015-05-01-preview'
+load_balancer_endpoint = 'https://management.azure.com/subscriptions/' + subscription_id + '/resourceGroups/azure1-westus/providers/Microsoft.Network/loadBalancers/azure1-st1-d1?api-version=2015-05-01-preview'
 
 print ctime(), ' - Validate Deployment'
 r = requests.get(load_balancer_endpoint, headers=headers)
